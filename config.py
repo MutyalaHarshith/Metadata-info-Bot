7ba1ea592e996c44552b6a544cfa9c4afcd5857e
Harshith <harshithxdev@gmail.com>
2025-03-22 20:00:18 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    # MetadataX client config
+    API_ID = os.environ.get("API_ID", "")
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    TELEGRAPH_TOKEN = os.environ.get("TELEGRAPH_TOKEN", "")
